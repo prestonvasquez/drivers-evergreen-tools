bb10497728687da4c07fd4048c0340f099052091
Preston Vasquez <prestonvasquez@icloud.com>
2025-06-25 18:17:15 -0600

    Bump mongoproxy

--- .evergreen/start-mongoproxy.sh
+++ .evergreen/start-mongoproxy.sh
@@ -34,7 +34,7 @@ test -x "${GOROOT}/bin/go" || {
   exit 1
 }
 
-"${GOROOT}/bin/go" install github.com/prestonvasquez/mongoproxy/cmd/mongoproxy@c128f19861b90c003f05c068b1a37636123b8634
+"${GOROOT}/bin/go" install github.com/prestonvasquez/mongoproxy/cmd/mongoproxy@12fd2299bbeae5e0244a529a69032f7abd3601bf
 
 if [[ -x "${GOPATH}/bin/mongoproxy" ]]; then
   echo "mongoproxy installed successfully to ${GOPATH}"
