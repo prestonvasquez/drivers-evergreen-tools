8e55185d707e5345fcc8337c74894e354dda99f7
Preston Vasquez <prestonvasquez@icloud.com>
2025-06-25 20:51:30 -0600

    Remove MONGODB_URI check

--- .evergreen/start-mongoproxy.sh
+++ .evergreen/start-mongoproxy.sh
@@ -44,9 +44,11 @@ else
 fi
 
 echo "Starting mongoproxy at ${MONGODB_URI}..."
-if [ -n "$MONGODB_URI" ]; then
-  exec "${GOPATH}/bin/mongoproxy" --target-uri "$MONGODB_URI"
-else
-  echo "Error: MONGODB_URI environment variable is not set." >&2
-  exit 1
-fi
+exec "${GOPATH}/bin/mongoproxy"
+
+#if [ -n "$MONGODB_URI" ]; then
+#  exec "${GOPATH}/bin/mongoproxy" --target-uri "$MONGODB_URI"
+#else
+#  echo "Error: MONGODB_URI environment variable is not set." >&2
+#  exit 1
+#fi
